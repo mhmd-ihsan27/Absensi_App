909082d3db066a6d23296575ecac1bf1b966b9d6
mhmd-ihsan27 <faisalihsan37@gmail.com>
2026-02-15 17:23:11 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDaftarNama = New System.Windows.Forms.TextBox()
+        Me.btnDaftar = New System.Windows.Forms.Button()
+        Me.dgvAbsensi = New System.Windows.Forms.DataGridView()
+        Me.btnAbsen = New System.Windows.Forms.Button()
+        Me.txtAbsenNama = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lstMahasiswa = New System.Windows.Forms.ListBox()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        CType(Me.dgvAbsensi, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtDaftarNama
+        '
+        Me.txtDaftarNama.Location = New System.Drawing.Point(216, 26)
+        Me.txtDaftarNama.Name = "txtDaftarNama"
+        Me.txtDaftarNama.Size = New System.Drawing.Size(218, 26)
+        Me.txtDaftarNama.TabIndex = 0
+        '
+        'btnDaftar
+        '
+        Me.btnDaftar.Location = New System.Drawing.Point(557, 22)
+        Me.btnDaftar.Name = "btnDaftar"
+        Me.btnDaftar.Size = New System.Drawing.Size(151, 35)
+        Me.btnDaftar.TabIndex = 1
+        Me.btnDaftar.Text = "Simpan"
+        Me.btnDaftar.UseVisualStyleBackColor = True
+        '
+        'dgvAbsensi
+        '
+        Me.dgvAbsensi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvAbsensi.Location = New System.Drawing.Point(12, 332)
+        Me.dgvAbsensi.Name = "dgvAbsensi"
+        Me.dgvAbsensi.RowHeadersWidth = 62
+        Me.dgvAbsensi.RowTemplate.Height = 28
+        Me.dgvAbsensi.Size = New System.Drawing.Size(803, 201)
+        Me.dgvAbsensi.TabIndex = 4
+        '
+        'btnAbsen
+        '
+        Me.btnAbsen.Location = New System.Drawing.Point(557, 159)
+        Me.btnAbsen.Name = "btnAbsen"
+        Me.btnAbsen.Size = New System.Drawing.Size(151, 35)
+        Me.btnAbsen.TabIndex = 5
+        Me.btnAbsen.Text = "Simpan"
+        Me.btnAbsen.UseVisualStyleBackColor = True
+        '
+        'txtAbsenNama
+        '
+        Me.txtAbsenNama.Location = New System.Drawing.Point(216, 159)
+        Me.txtAbsenNama.Name = "txtAbsenNama"
+        Me.txtAbsenNama.Size = New System.Drawing.Size(218, 26)
+        Me.txtAbsenNama.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 20)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Masukan Nama"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 165)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Absen"
+        '
+        'lstMahasiswa
+        '
+        Me.lstMahasiswa.FormattingEnabled = True
+        Me.lstMahasiswa.ItemHeight = 20
+        Me.lstMahasiswa.Location = New System.Drawing.Point(812, 332)
+        Me.lstMahasiswa.Name = "lstMahasiswa"
+        Me.lstMahasiswa.Size = New System.Drawing.Size(373, 204)
+        Me.lstMahasiswa.TabIndex = 9
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(1034, 542)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(151, 35)
+        Me.btnHapus.TabIndex = 10
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1181, 706)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.lstMahasiswa)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtAbsenNama)
+        Me.Controls.Add(Me.btnAbsen)
+        Me.Controls.Add(Me.dgvAbsensi)
+        Me.Controls.Add(Me.btnDaftar)
+        Me.Controls.Add(Me.txtDaftarNama)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvAbsensi, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtDaftarNama As TextBox
+    Friend WithEvents btnDaftar As Button
+    Friend WithEvents dgvAbsensi As DataGridView
+    Friend WithEvents btnAbsen As Button
+    Friend WithEvents txtAbsenNama As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lstMahasiswa As ListBox
+    Friend WithEvents btnHapus As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,114 @@
+﻿Imports System.IO
+Imports System.Data
+
+Public Class Form1
+    Dim tabelAbsen As New DataTable()
+    Dim fileMahasiswa As String = "daftar_mahasiswa.txt"
+    Dim fileAbsensi As String = "riwayat_absensi.txt"
+    Dim listMahasiswa As New List(Of String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PersiapkanTabel()
+        MuatDaftarMahasiswa()
+        MuatRiwayatAbsen()
+    End Sub
+
+    Private Sub PersiapkanTabel()
+        tabelAbsen.Columns.Add("Nama", GetType(String))
+        tabelAbsen.Columns.Add("Waktu", GetType(String))
+        dgvAbsensi.DataSource = tabelAbsen
+        dgvAbsensi.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+    End Sub
+
+    Private Sub btnDaftar_Click(sender As Object, e As EventArgs) Handles btnDaftar.Click
+        Dim namaBaru As String = txtDaftarNama.Text.Trim()
+
+        If namaBaru = "" Then
+            MessageBox.Show("Nama tidak boleh kosong!", "Peringatan")
+        ElseIf listMahasiswa.Contains(namaBaru) Then
+            MessageBox.Show("Mahasiswa ini sudah terdaftar dalam sistem.")
+        Else
+            listMahasiswa.Add(namaBaru)
+            SimpanMahasiswaKeFile()
+            UpdateTampilanList()
+            txtDaftarNama.Clear()
+        End If
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If lstMahasiswa.SelectedIndex <> -1 Then
+            Dim namaDihapus As String = lstMahasiswa.SelectedItem.ToString()
+
+            Dim konfirmasi = MessageBox.Show("Hapus '" & namaDihapus & "'? Semua riwayat absennya juga akan dihapus.",
+                                             "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+            If konfirmasi = DialogResult.Yes Then
+                listMahasiswa.Remove(namaDihapus)
+                SimpanMahasiswaKeFile()
+
+                For i As Integer = tabelAbsen.Rows.Count - 1 To 0 Step -1
+                    If tabelAbsen.Rows(i)("Nama").ToString() = namaDihapus Then
+                        tabelAbsen.Rows.RemoveAt(i)
+                    End If
+                Next
+
+                SimpanAbsenKeFile()
+                UpdateTampilanList()
+            End If
+        Else
+            MessageBox.Show("Pilih nama di daftar terlebih dahulu untuk dihapus.")
+        End If
+    End Sub
+
+    Private Sub btnAbsen_Click(sender As Object, e As EventArgs) Handles btnAbsen.Click
+        Dim namaInput As String = txtAbsenNama.Text.Trim()
+
+        If listMahasiswa.Contains(namaInput) Then
+            Dim waktu As String = DateTime.Now.ToString("HH:mm:ss")
+            tabelAbsen.Rows.Add(namaInput, waktu)
+            SimpanAbsenKeFile()
+            txtAbsenNama.Clear()
+        Else
+            MessageBox.Show("Maaf, '" & namaInput & "' tidak terdaftar. Silakan daftar dulu!", "Akses Ditolak")
+        End If
+    End Sub
+
+    Private Sub UpdateTampilanList()
+        lstMahasiswa.Items.Clear()
+        For Each mhs In listMahasiswa
+            lstMahasiswa.Items.Add(mhs)
+        Next
+    End Sub
+
+    Private Sub MuatDaftarMahasiswa()
+        If File.Exists(fileMahasiswa) Then
+            Dim baris As String() = File.ReadAllLines(fileMahasiswa)
+            For Each n In baris
+                If Not String.IsNullOrWhiteSpace(n) Then listMahasiswa.Add(n)
+            Next
+            UpdateTampilanList()
+        End If
+    End Sub
+
+    Private Sub MuatRiwayatAbsen()
+        If File.Exists(fileAbsensi) Then
+            Dim baris As String() = File.ReadAllLines(fileAbsensi)
+            For Each b In baris
+                Dim data = b.Split("|"c)
+                If data.Length = 2 Then tabelAbsen.Rows.Add(data(0), data(1))
+            Next
+        End If
+    End Sub
+
+    Private Sub SimpanMahasiswaKeFile()
+        File.WriteAllLines(fileMahasiswa, listMahasiswa)
+    End Sub
+
+    Private Sub SimpanAbsenKeFile()
+        Using sw As New StreamWriter(fileAbsensi)
+            For Each row As DataRow In tabelAbsen.Rows
+                sw.WriteLine(row("Nama").ToString() & "|" & row("Waktu").ToString())
+            Next
+        End Using
+    End Sub
+End Class
